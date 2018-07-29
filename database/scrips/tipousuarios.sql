ef418802f1e3618abe255fdaf070c33812de2999
jozsefProgrammer <38340913+jozsefProgrammer@users.noreply.github.com>
2018-07-29 12:26:20 -0600

    insert
    tipousuarios

---
+++ database/scrips/tipousuarios.sql
@@ -0,0 +1,5 @@
+INSERT INTO `pproyectodb`.`tipousuarios` (`nombre`, `permissions`, `created_at`, `updated_at`) VALUES ('Administrador', '{\"create-ct\":true,\"update-ct\":true,\"state-ct\":true,\"create-tm\":true,\"update-tm\":true,\"state-tm\":true,\"create-aa\":true,\"update-aa\":true,\"state-aa\":true}', '2018-07-17 01:15:42', '2018-07-17 01:15:42');
+
+INSERT INTO `pproyectodb`.`tipousuarios` (`nombre`, `permissions`, `created_at`, `updated_at`) VALUES ('Administrador Centro Acopio', '{\"create-tm\":true}','2018-07-17 01:15:42', '2018-07-17 01:15:42');
+
+INSERT INTO `pproyectodb`.`tipousuarios` (`nombre`, `created_at`, `updated_at`) VALUES ('Cliente', '2018-07-17 01:15:42', '2018-07-17 01:15:42');
